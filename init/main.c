8065c73e358a8b0a261233ccfd3fdb93e37ac2ac
Daniel Golle <daniel@makrotopia.org>
2015-03-11 16:04:15 +0100

    921-use_preinit_as_init

--- init/main.c
+++ init/main.c
@@ -974,7 +974,8 @@ static int __ref kernel_init(void *unused)
 		       execute_command, ret);
 #endif
 	}
-	if (!try_to_run_init_process("/sbin/init") ||
+	if (!try_to_run_init_process("/etc/preinit") ||
+	    !try_to_run_init_process("/sbin/init") ||
 	    !try_to_run_init_process("/etc/init") ||
 	    !try_to_run_init_process("/bin/init") ||
 	    !try_to_run_init_process("/bin/sh"))
